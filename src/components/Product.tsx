e1a3d27a09ff18821e3615f98cc5264298b3a336
DipakDai <dipakkumal673@gmail.com>
2023-12-04 16:50:38 +0545

    minor changes

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,20 @@
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Container, Divider, Flex, Heading, Input, Select, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Select,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -36,58 +52,63 @@ export default function ProductPage() {
   console.log(product);
   return (
     <>
-     <Container>
-     <Flex gap="25px">
-        <Box>
-          <Input type="text" placeholder="Search product here ....." />
-        </Box>
-        <Box>
-          <Select placeholder="Select the Category">
-            {categories?.map((categories: [], index: number) => (
-              <option value={categories} key={index}>
-                {categories}
-              </option>
-            ))}
-          </Select>
-        </Box>
-      </Flex>
-     </Container>
+      <Container mt="14px">
+        <Flex gap="25px">
+          <Box>
+            <Input type="text" placeholder="Search product here ....." />
+          </Box>
+          <Box>
+            <Select placeholder="Select the Category">
+              {categories?.map((categories: [], index: number) => (
+                <option value={categories} key={index}>
+                  {categories}
+                </option>
+              ))}
+            </Select>
+          </Box>
+        </Flex>
+      </Container>
 
-      <Flex>
-        <Box>
-          <Card maxW="sm">
-            <CardBody>
-              <img
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                // borderRadius="lg"
-              />
-              <Stack mt="6" spacing="3">
-                <Heading size="md">Living room Sofa</Heading>
-                <Box>
-                  This sofa is perfect for modern tropical spaces, baroque
-                  inspired spaces, earthy toned spaces and for people who love a
-                  chic design with a sprinkle of vintage design.
-                </Box>
-                <Box color="blue.600" fontSize="2xl">
-                  $450
-                </Box>
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Buy now
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Add to cart
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-        </Box>
-      </Flex>
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(400px, 1fr))"
+        px="15px"
+        mt="20px"
+      >
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <p>View a summary of all your customers over the last month.</p>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <p>View a summary of all your customers over the last month.</p>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <p>View a summary of all your customers over the last month.</p>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
     </>
   );
 }
