688439d459dc48285d9bdffb13e032c518ac4233
DipakDai <dipakkumal673@gmail.com>
2023-12-04 16:23:30 +0545

    feat: fetched and show category

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,13 +1,41 @@
-import { Box, Container, Input } from "@chakra-ui/react";
+import { Box, Container, Flex, Input, Select } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+export default function ProductPage() {
+  const { data: product } = useQuery({
+    queryKey: ["product"],
+    queryFn: async () => {
+      return await fetch("https://dummyjson.com/products").then((res) =>
+        res.json()
+      );
+    },
+  });
 
-
-export default function ProductPage(){
-    return (
-        <Container marginTop="50px">
-
-            <Box>
-                <Input type="text" placeholder="Search product here ....." />
-            </Box>
-        </Container>
-    )
+  const { data: categories } = useQuery({
+    queryKey: ['categories'],
+    queryFn: async () => {
+        return await fetch('https://dummyjson.com/products/categories').then((res) =>
+            res.json()
+        );
+    },
+});
+  console.log(product);
+  return (
+    <Container marginTop="50px">
+      <Flex gap="25px">
+        <Box>
+          <Input type="text" placeholder="Search product here ....." />
+        </Box>
+        <Box>
+          <Select placeholder="Select the Category">
+           {
+            categories?.map((categories:[],index:number) =>(
+                <option value={categories} key={index}>{categories}</option>
+            ))
+           }
+          </Select>
+        </Box>
+      </Flex>
+    </Container>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
