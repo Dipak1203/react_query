06f92d58104ad996156fe2f84e3ccd2c2c6ee2ed
DipakDai <dipakkumal673@gmail.com>
2023-12-04 15:42:00 +0545

    project started

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,96 @@
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from '@chakra-ui/react'
+import logo from '../assets/dipakLogo.png'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+
+// interface Props {
+//   children: React.ReactNode
+// }
+
+// const NavLink = (props: Props) => {
+//   const { children } = props
+
+//   return (
+//     <Box
+//       as="a"
+//       px={2}
+//       py={1}
+//       rounded={'md'}
+//       _hover={{
+//         textDecoration: 'none',
+//         bg: useColorModeValue('gray.200', 'gray.700'),
+//       }}
+//       href={'#'}>
+//       {children}
+//     </Box>
+//   )
+// }
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={5} py={3}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>
+            <img src={logo} alt="logo" width={"80px"}/>
+          </Box>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.githubusercontent.com/u/67213707?v=4'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.githubusercontent.com/u/67213707?v=4'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>DIPAK KUMAL</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  )
+}
